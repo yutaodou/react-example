cda71d4124468b6dd51d6fcfc23b77e54bf45018
Dou Yutao <yutao.dou@rea-group.com>
2016-07-26 21:33:19 +0800

    Separate presentational component from container

---
+++ src/components/Prompt.js
@@ -0,0 +1,28 @@
+var React = require('react');
+var background = require('../styles/index');
+
+var Prompt = React.createClass({
+    render: function(){
+        return (
+            <div className = "jumbotron col-sm-6 col-sm-offset-3 text-center" style={background}>
+                <h1>{this.props.header}</h1>
+                <div className = "col-sm-12">
+                    <form onSubmit = {this.props.onSubmitUser}>
+                        <div className="form-group">
+                            <input
+                                className="form-control"
+                                placeholder="Github username"
+                                onChange = {this.props.onUpdateUser}
+                                value = {this.props.username}
+                                type="text"/>
+                        </div>
+                        <div className="form-group col-sm-4 col-sm-offset-3">
+                            <button className="btn btn-block btn-success" type="submit">Continue</button>
+                        </div>
+                    </form>
+                </div>
+            </div>        );
+    }
+});
+
+module.exports = Prompt;
--- src/containers/PromptContainer.js
+++ src/containers/PromptContainer.js
@@ -1,5 +1,5 @@
 var React = require('react');
-var background = require('../styles/index');
+var Prompt = require('../components/Prompt');
 
 var PromptContainer = React.createClass({
     contextTypes: {
@@ -12,7 +12,7 @@ var PromptContainer = React.createClass({
         };
     },
 
-    onUpdateUser: function(e){
+    handleUpdateUser: function(e){
         e.preventDefault();
 
         var username = this.state.username;
@@ -21,7 +21,7 @@ var PromptContainer = React.createClass({
         });
     },
 
-    onSubmitUser: function(e){
+    handleSubmitUser: function(e){
         e.preventDefault();
 
         var username = this.state.username;
@@ -44,24 +44,11 @@ var PromptContainer = React.createClass({
 
     render: function(){
         return (
-            <div className = "jumbotron col-sm-6 col-sm-offset-3 text-center" style={background}>
-                <h1>{this.props.route.header}</h1>
-                <div className = "col-sm-12">
-                    <form onSubmit = {this.onSubmitUser}>
-                        <div className="form-group">
-                            <input
-                                className="form-control"
-                                placeholder="Github username"
-                                onChange = {this.onUpdateUser}
-                                value = {this.state.username}
-                                type="text"/>
-                        </div>
-                        <div className="form-group col-sm-4 col-sm-offset-3">
-                            <button className="btn btn-block btn-success" type="submit">Continue</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+            <Prompt
+                onSubmitUser = {this.handleSubmitUser}
+                onUpdateUser = {this.handleUpdateUser}
+                header = {this.props.route.header}
+                username= {this.state.username} />
         );
     }
 });
